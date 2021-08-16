4a47bdbba14b2d071e36e68192ee66306a049330
Laky730 <benibubu2@gmail.com>
2021-08-16 13:50:38 +0200

    Added shaders, which add color and other cool stuff :)

--- main.py
+++ main.py
@@ -1,20 +1,69 @@
 import pyglet
+import ctypes
+
+import shader
 
 pyglet.options["shadow_window"] = False
 pyglet.options["debug_gl"] = False
 
 import pyglet.gl as gl
 
+vertex_positions = [ # 3d coordinates for each vertex
+	-0.5,  0.5, 1.0,
+	-0.5, -0.5, 1.0,
+	 0.5, -0.5, 1.0,
+	 0.5,  0.5, 1.0,
+]
+
+indices = [
+	0, 1, 2, # first triangle
+	0, 2, 3, # second triangle
+]
+
 class Window(pyglet.window.Window):
     def __init__(self, **args):
         super(Window,self).__init__(**args)
 
+        self.vao = gl.GLuint(0)
+        gl.glGenVertexArrays(1, ctypes.byref(self.vao))
+        gl.glBindVertexArray(self.vao)
+
+        # create vertex buffer object
+
+        self.vbo = gl.GLuint(0)
+        gl.glGenBuffers(1, ctypes.byref(self.vbo))
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vbo)
+
+        gl.glBufferData(gl.GL_ARRAY_BUFFER,
+                        ctypes.sizeof(gl.GLfloat * len(vertex_positions)),
+                        (gl.GLfloat * len(vertex_positions))(*vertex_positions),
+                        gl.GL_STATIC_DRAW)
+
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, 0)
+        gl.glEnableVertexAttribArray(0)
+
+        # create index buffer object
+
+        self.ibo = gl.GLuint(0)
+        gl.glGenBuffers(1, self.ibo)
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.ibo)
+
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER,
+                        ctypes.sizeof(gl.GLuint * len(indices)),
+                        (gl.GLuint * len(indices))(*indices),
+                        gl.GL_STATIC_DRAW)
+
+        self.shader = shader.Shader("vert.glsl", "frag.glsl")
+        self.shader.use()
+
     def on_draw(self):
         gl.glClearColor(0.0,0.0,0.0,1.0)
         self.clear()
+        gl.glDrawElements(gl.GL_TRIANGLES, len(indices), gl.GL_UNSIGNED_INT, None)  # draw bound buffers to the screen
 
     def on_resize(self,width,height):
         print(f"Resize {width} * {height}")  # print out window size
+        gl.glViewport(0, 0, width, height)  # resize the actual OpenGL viewport
 
 class Game:
     def __init__(self):
