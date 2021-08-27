ea2c28cab6beae1ca15ef8ca13d3215ed63a6675
Laky730 <benibubu2@gmail.com>
2021-08-27 12:02:31 +0200

    Added textures and block models! :D

--- frag.glsl
+++ frag.glsl
@@ -1,9 +1,13 @@
+
 #version 330
 
 out vec4 fragment_colour;
 
+uniform sampler2DArray texture_array_sampler; // create our texture array sampler uniform
+
 in vec3 local_position;
+in vec3 interpolated_tex_coords; // interpolated texture coordinates
 
 void main(void) {
-	fragment_colour = vec4(local_position / 2.0 + 0.5, 1.0); // set the output colour based on the vertex position
+	fragment_colour = texture(texture_array_sampler, interpolated_tex_coords); // sample our texture array with the interpolated texture coordinates
 }
--- main.py
+++ main.py
@@ -13,24 +13,31 @@ import shader
 import matrix  # import matrix.py file
 
 
-vertex_positions = [  # set the Z component to 0.0 so that our object is centered
-    -0.5, 0.5, 0.0,
-    -0.5, -0.5, 0.0,
-    0.5, -0.5, 0.0,
-    0.5, 0.5, 0.0,
-]
+import block_type # import block_type.py file
+import textureManager # import texture_manager.py file
 
-indices = [
-    0, 1, 2,  # first triangle
-    0, 2, 3,  # second triangle
-]
+
+
 
 
 class Window(pyglet.window.Window):
     def __init__(self, **args):
         super().__init__(**args)
 
+        self.texture_manager = textureManager.Texture_manager(16, 16,256)  # create our texture manager (256 textures that are 16 x 16 pixels each)
 
+
+
+        self.cobblestone = block_type.Block_type(self.texture_manager,"cobblestone", {"all": "cobblestone"})
+        self.dirt = block_type.Block_type(self.texture_manager,"dirt", {"all": "dirt"})
+        self.sand = block_type.Block_type(self.texture_manager,"sand", {"all": "sand"})
+        self.stone = block_type.Block_type(self.texture_manager,"stone", {"all": "stone"})
+        self.planks = block_type.Block_type(self.texture_manager,"planks", {"all": "planks"})
+        self.lakyBlock = block_type.Block_type(self.texture_manager,"lakyBlock", {"all": "lakyBlock"})
+        self.ironBlock = block_type.Block_type(self.texture_manager,"ironBlock", {"all": "ironBlock"})
+        self.grassBlock = block_type.Block_type(self.texture_manager,"grassBlock", {"top": "grassBlock_top", "bottom": "dirt", "sides": "grassBlock_side"})
+
+        self.texture_manager.generate_mipmaps()  # generate mipmaps for our texture manager's texture
 
         # create vertex array object
 
@@ -38,19 +45,37 @@ class Window(pyglet.window.Window):
         gl.glGenVertexArrays(1, ctypes.byref(self.vao))
         gl.glBindVertexArray(self.vao)
 
-        # create vertex buffer object
+        # create vertex position vbo
 
-        self.vbo = gl.GLuint(0)
-        gl.glGenBuffers(1, ctypes.byref(self.vbo))
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vbo)
+        self.vertex_position_vbo = gl.GLuint(0)
+        gl.glGenBuffers(1, ctypes.byref(self.vertex_position_vbo))
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vertex_position_vbo)
 
-        gl.glBufferData(gl.GL_ARRAY_BUFFER,
-                        ctypes.sizeof(gl.GLfloat * len(vertex_positions)),
-                        (gl.GLfloat * len(vertex_positions))(*vertex_positions),
-                        gl.GL_STATIC_DRAW)
+        gl.glBufferData(
+            gl.GL_ARRAY_BUFFER,
+            ctypes.sizeof(gl.GLfloat * len(self.grassBlock.vertex_positions)),
+            (gl.GLfloat * len(self.grassBlock.vertex_positions))(*self.grassBlock.vertex_positions),
+            # use grass block's vertex positions
+            gl.GL_STATIC_DRAW)
 
         gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, 0)
         gl.glEnableVertexAttribArray(0)
+
+        # create tex coord vbo
+
+        self.tex_coord_vbo = gl.GLuint(0)
+        gl.glGenBuffers(1, ctypes.byref(self.tex_coord_vbo))
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.tex_coord_vbo)
+
+        gl.glBufferData(
+            gl.GL_ARRAY_BUFFER,
+            ctypes.sizeof(gl.GLfloat * len(self.grassBlock.tex_coords)),
+            (gl.GLfloat * len(self.grassBlock.tex_coords))(*self.grassBlock.tex_coords),
+            # use grass block's texture coordinates positions
+            gl.GL_STATIC_DRAW)
+
+        gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, 0)
+        gl.glEnableVertexAttribArray(1)
 
         # create index buffer object
 
@@ -58,21 +83,24 @@ class Window(pyglet.window.Window):
         gl.glGenBuffers(1, self.ibo)
         gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.ibo)
 
-        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER,
-                        ctypes.sizeof(gl.GLuint * len(indices)),
-                        (gl.GLuint * len(indices))(*indices),
-                        gl.GL_STATIC_DRAW)
+        gl.glBufferData(
+            gl.GL_ELEMENT_ARRAY_BUFFER,
+            ctypes.sizeof(gl.GLuint * len(self.grassBlock.indices)),
+            (gl.GLuint * len(self.grassBlock.indices))(*self.grassBlock.indices),  # use grass block's indices
+            gl.GL_STATIC_DRAW)
 
         # create shader
 
         self.shader = shader.Shader("vert.glsl", "frag.glsl")
-        self.shader_matrix_location = self.shader.find_uniform(b"matrix")  # get the shader matrix uniform location
+        self.shader_matrix_location = self.shader.find_uniform(b"matrix")
+        self.shader_sampler_location = self.shader.find_uniform(
+            b"texture_array_sampler")  # find our texture array sampler's uniform
         self.shader.use()
 
         # create matrices
 
-        self.mv_matrix = matrix.Matrix()  # modelview
-        self.p_matrix = matrix.Matrix()  # projection
+        self.mv_matrix = matrix.Matrix()
+        self.p_matrix = matrix.Matrix()
 
         self.x = 0  # temporary variable
         pyglet.clock.schedule_interval(self.update, 1.0 / 120)  # call update function every 60th of a second
@@ -81,7 +109,7 @@ class Window(pyglet.window.Window):
 
 
         self.x += delta_time  # increment self.x consistently
-        self.set_caption("LakyCraft Python Alpha 0.4 | FPS: " + str(round(pyglet.clock.get_fps())))
+        self.set_caption("LakyCraft Python Alpha 0.5 | FPS: " + str(round(pyglet.clock.get_fps())))
 
     def on_draw(self):
         # create projection matrix
@@ -89,25 +117,34 @@ class Window(pyglet.window.Window):
         self.p_matrix.load_identity()
         self.p_matrix.perspective(90, float(self.width) / self.height, 0.1, 500)
 
-        # create model view matrix
+        # create modelview matrix
 
         self.mv_matrix.load_identity()
-        self.mv_matrix.translate(0, 0, -1)
+        self.mv_matrix.translate(0, 0, -3)
         self.mv_matrix.rotate_2d(self.x, math.sin(self.x / 3 * 2) / 2)
 
-        # multiply the two matrices together and send to the shader program
+        # modelviewprojection matrix
 
         mvp_matrix = self.p_matrix * self.mv_matrix
         self.shader.uniform_matrix(self.shader_matrix_location, mvp_matrix)
 
+        # bind textures
+
+        gl.glActiveTexture(gl.GL_TEXTURE0)  # set our active texture unit to the first texture unit
+        gl.glBindTexture(gl.GL_TEXTURE_2D_ARRAY,
+                         self.texture_manager.texture_array)  # bind our texture manager's texture
+        gl.glUniform1i(self.shader_sampler_location,
+                       0)  # tell our sampler our texture is bound to the first texture unit
+
         # draw stuff
 
+        gl.glEnable(gl.GL_DEPTH_TEST)  # enable depth testing so faces are drawn in the right order
         gl.glClearColor(0.0, 0.0, 0.0, 1.0)
         self.clear()
 
         gl.glDrawElements(
             gl.GL_TRIANGLES,
-            len(indices),
+            len(self.grassBlock.indices),
             gl.GL_UNSIGNED_INT,
             None)
 
@@ -118,7 +155,7 @@ class Window(pyglet.window.Window):
 
 class Game:
     def __init__(self):
-        self.config = gl.Config(double_buffer=True, major_version=3)
+        self.config = gl.Config(double_buffer=True, major_version=3,depth_size = 1)
         self.window = Window(config=self.config, width=800, height=600, caption="LakyCraft Python Alpha 0.4", resizable=True,
                              vsync=False)
 
